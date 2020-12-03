3c651920c79bc6d043a4bd1a07581021eef3a460
Landon Holmes <lholmes@quantumedgetechnology.com>
2020-12-02 18:32:14 -0600

    -solution for day 1 problems 1 and 2

--- 01/01-2.py
+++ 01/01-2.py
@@ -8,12 +8,13 @@ with open('input.txt') as f:
     for line in lines:
         reportNumbers.append(int(line))
 
-for combo in itertools.combinations(reportNumbers, 2):
+for combo in itertools.combinations(reportNumbers, 3):
     first = combo[0]
     second = combo[1]
+    third = combo[2]
 
-    if first + second == 2020:
-        solution = first * second
+    if first + second + third == 2020:
+        solution = first * second * third
         break
 
-print(solution) # 987339
+print(solution) # 259521570
